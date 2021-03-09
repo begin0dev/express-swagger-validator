3bdab3234baedf20cbea49971870efb0f453fe23
begin0dev <prosonic1@gmail.com>
2021-03-09 23:46:23 +0900

    ADD: generator open api json

--- lib/swagger-generator.ts
+++ lib/swagger-generator.ts
@@ -1,11 +1,11 @@
 import * as OpenApi from 'openapi3-ts';
 import { Express } from 'express';
 import swaggerUi, { SwaggerOptions } from 'swagger-ui-express';
-import { TObject, TSchema } from '@serverless-seoul/typebox';
+import { setWith } from 'lodash';
 
 import { routePathToSwaggerPath, requestKeyMap } from './helper';
 import { getEndPoints } from './end-points';
-import { ParametersKey } from './types';
+import { ParametersKey, DocSchema } from './types';
 
 interface ISwaggerOptions {
   info: OpenApi.InfoObject;
@@ -33,38 +33,57 @@ class SwaggerGenerator {
       const routePath = routePathToSwaggerPath(endPoint.path);
 
       const { additionalProperties, body, description, summary, tags, ...parameters } = endPoint.schema || {};
-      const pathsObject: OpenApi.OperationObject = {
-        description,
-        summary,
-        tags,
-        responses: {},
-      };
+      const pathsObject: OpenApi.OperationObject = { responses: {} };
+      if (summary) pathsObject.summary = summary;
+      if (description) pathsObject.description = description;
+      if (tags) pathsObject.tags = tags;
 
       // set openapi parameter 'cookie', 'header', 'path', 'query'
       if (Object.keys(parameters).length > 0) {
         pathsObject.parameters = Object.entries(parameters).reduce(
-          (acc: OpenApi.ParameterObject[], [requestKey, keySchema]: [ParametersKey, TObject<any>]) =>
+          (
+            acc: OpenApi.ParameterObject[],
+            [requestKey, requestKeySchema]: [ParametersKey, DocSchema<any>[ParametersKey]],
+          ) =>
             acc.concat(
-              Object.entries(keySchema.properties).map(
-                ([name, { description: schemaDesc, ...schema }]: [
-                  string,
-                  TSchema,
-                ]): OpenApi.ParameterObject => ({
+              Object.entries(requestKeySchema.properties).map(
+                ([name, { description: schemaDesc, ...schema }]: [string, any]): OpenApi.ParameterObject => ({
                   name,
+                  schema,
                   in: requestKeyMap[requestKey],
                   description: schemaDesc,
-                  required: (keySchema.required || []).includes(name),
+                  required: (requestKeySchema.required || []).includes(name),
                 }),
               ),
             ),
           [],
         );
       }
-      console.log(pathsObject);
+
+      if (body) {
+        const { required, properties } = body;
+        pathsObject.requestBody = {
+          content: {
+            'application/json': {
+              schema: {
+                type: 'object',
+                properties: properties as OpenApi.SchemaObject,
+                required: (required || []) as string[],
+              },
+            },
+          },
+          required: true,
+        };
+      }
+
+      endPoint.methods.forEach((method) => {
+        this.openApiJSON.paths = setWith(this.openApiJSON.paths, [routePath, method], pathsObject, Object);
+      });
     });
   };
 
   setSwaggerUi = (path: string, app: Express, options?: SwaggerOptions) => {
+    this.generatorOpenApiJSON(app);
     app.use(path, swaggerUi.serve, swaggerUi.setup(this.openApiJSON, options));
   };
 }
--- lib/types/index.ts
+++ lib/types/index.ts
@@ -1,17 +1,22 @@
-import { TObject } from '@serverless-seoul/typebox';
+import { JSONSchemaType } from 'ajv';
+import { TObject, TProperties } from '@serverless-seoul/typebox';
 
 import { requestKeyMap } from '../helper';
 
 export type RoutesKey = keyof typeof requestKeyMap;
 export type ParametersKey = Exclude<RoutesKey, 'body'>;
 
+export type JSONSchema<T extends Record<string, any>> = {
+  [K in keyof T]: JSONSchemaType<T[K]> & { description?: string };
+};
+
 export type DocSchema<T extends Partial<Record<RoutesKey, any>>> = {
-  [K in RoutesKey]?: TObject<T[K]>;
-} & {
   summary: string;
   additionalProperties?: boolean;
   description?: string;
   tags?: string[];
+} & {
+  [K in RoutesKey]?: JSONSchema<T[K]> | TObject<TProperties>;
 };
 
 export type EndPoint = { path: string; methods: string[]; schema: DocSchema<any> };
